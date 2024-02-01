fb667241bc225aef3c42b89b86dff7fe0adfdde8
Dmitriy Menshikov <d.menshikov@iberica.kz>
2024-02-01 19:17:12 +0300

    Initial Commit

---
+++ ImageFeed/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ImageFeed
+//
+//  Created by Dmitriy Menshikov on 1.02.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
