e5a595385b5f67f638760c6b61558b4b13ee5021
Dmitriy Menshikov <d.menshikov@iberica.kz>
2024-03-03 01:07:08 +0300

    Список изменений: 1) Сверстан экран авторизации

---
+++ ImageFeed/Auth/AuthViewController.swift
@@ -0,0 +1,17 @@
+//
+//  AuthViewController.swift
+//  ImageFeed
+//
+//  Created by Dmitriy Menshikov on 3.03.24.
+//
+
+import UIKit
+
+final class AuthViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+}
---
+++ ImageFeed/Auth/WebViewViewController.swift
@@ -0,0 +1,18 @@
+//
+//  WebViewViewController.swift
+//  ImageFeed
+//
+//  Created by Dmitriy Menshikov on 3.03.24.
+//
+
+import UIKit
+
+final class WebViewViewController: UIViewController {
+    
+    private let segueID = "ShowWebView"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
